3dbf92937cbb68fd0995cd0d60584c9e8afe0019
mohamedLemtouni <lemtouni7@gmail.com>
2024-10-07 12:59:45 +0200

    Update cesar.py

--- cesar.py
+++ cesar.py
@@ -3,10 +3,14 @@ from ssi_lib import *
 ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def cesar(dep):
-    result = shift(a=ALPHABET.index(dep),n=2,length=3)
-     
+    result = shift(dep,n=2,length=4)
+    print(chr(result))
     return ALPHABET[result]
 
 
 def advanced_cesar():
     pass
+
+
+
+cesar("A")
